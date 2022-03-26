c9d07aa6ebf824da9dbfa61c2ad67476e77bca8b
Stewart Wallace <stewartpwallace@gmail.com>
2022-03-26 10:40:24 -0400

    Added ability to create new sql table from pokemon data and populate table with JSON data

--- src/pokemon_writer.py
+++ src/pokemon_writer.py
@@ -2,7 +2,6 @@
 Downloads file from Poke API
 """
 
-
 class PokemonWriter:
     """
     Downloads JSON file from Poke API endpoint
@@ -10,63 +9,119 @@ class PokemonWriter:
 
     @classmethod
     def convert_pokemon_json_to_sql(cls, pokemon_json):
+        """
+        Create SQL file from pokemon data
+        """
 
-        """
-        Parse pokemon json to remove unnecessary data and extract moves set
-        """
-        return pokemon_json
-    
+        pokemon_columns = [("pokemon_name", "varchar"),("id", "int"),  ("height", "varchar"),\
+                        ("weight", "int"),("ability", "varchar"),("species", "varchar"),\
+                        ("stats", "varchar"),("primary_type", "varchar"),\
+                        ("secondary_type", "varchar")]
+        table_name = "Pokemon"
+        primary_key = "pokemon_name"
+        sql_pokemon_creation = cls.create_table(table_name, pokemon_columns)
+        sql_pokemon_creation = cls.create_primary_key(primary_key,table_name,sql_pokemon_creation)
+        sql_pokemon_creation = cls.populate_table(pokemon_json, table_name, sql_pokemon_creation)
+        return sql_pokemon_creation
 
     @classmethod
     def create_table(cls, table_name, columns):
+        """
+        Create SQL table using table and columns
+        """
 
         column_name = 0
         column_datatype = 1
-        sql_creation_string = "CREATE TABLE " + table_name + " ("
+        sql_creation = "CREATE TABLE " + table_name + " ("
 
         for column in columns:
-            sql_creation_string = sql_creation_string + "\t" + column[column_name] + \
+            sql_creation = sql_creation + "\t" + column[column_name] + \
             " " + column[column_datatype] + ",\n"
 
-        # Remove the last comma. Not needed for last column
-        last_comma_index = sql_creation_string.rfind(',')
-        sql_creation_string = sql_creation_string[0 : last_comma_index : ] + sql_creation_string[last_comma_index + 1 : :]
-        
-        sql_creation_string = sql_creation_string + ");\n"
+        sql_creation = cls.remove_last_comma(sql_creation, False)
 
-        return sql_creation_string
-    
+        sql_creation = sql_creation + ");\n"
+
+        return sql_creation
+
     @classmethod
     def create_primary_keys(cls,keys_tables, sql_creation_string):
+        """
+        Create set of SQL primary keys using Alter table method.
+        """
+
         for key_table in keys_tables:
             sql_creation_string = sql_creation_string + "ALTER TABLE " + key_table[0] +\
                              " ADD PRIMARY KEY (" + key_table[1] + ");\n"
-        
+
+        return sql_creation_string
+
+    @classmethod
+    def create_primary_key(cls, primary_key, table_name, sql_creation_string):
+        """
+        Create single primary key
+        """
+
+        sql_creation_string = sql_creation_string + "ALTER TABLE " + table_name + \
+                             " ADD PRIMARY KEY (" + primary_key + ");\n"
+
         return sql_creation_string
 
     @classmethod
     def create_relationships(cls, keys_tables, sql_creation_string):
+        """
+        Create SQL relationships using alter table method.
+        """
 
         for key_table in keys_tables:
             sql_creation_string = sql_creation_string + "ALTER TABLE " + key_table[0] + \
-            " ADD FOREIGN KEY (" +key_table[1] + ") REFERENCES " + key_table[2] + " (" +key_table[3] + ");\n"
- 
+            " ADD FOREIGN KEY (" +key_table[1] + ") REFERENCES " + key_table[2] + \
+            " (" +key_table[3] + ");\n"
+
         return sql_creation_string
 
     @classmethod
-    def populate_table(cls, values, table_name, sql_creation_string):
+    def populate_table(cls, pokemon_json, table_name, sql_creation_string):
+        """
+        Populate SQL table using values only
+        """
 
         value_string = "("
-        for value in values:
+        value_index = 1
+
+        for element in pokemon_json.items():
+            value = element[value_index]
+
+            if not isinstance(value, str):
+                value = str(value)
+
+            value = value.strip()
             value_string = value_string + value +", "
 
-        # Remove the last comma. Not needed for last column
-        last_comma_index = value_string.rfind(',')
-        value_string = value_string[0 : last_comma_index : ] + value_string[last_comma_index + 1 : :]
+        value_string = cls.remove_last_comma(value_string, True)
 
-        value_string = ")"
-        sql_creation_string = "INSERT INTO "+ table_name + " VALUES " + value_string 
- 
+        value_string = value_string + ");"
+        sql_creation_string = sql_creation_string + "INSERT INTO "+ table_name +\
+                             " VALUES " + value_string
+
         return sql_creation_string
 
-        
+    @classmethod
+    def remove_last_comma(cls, sql_creation, with_extra_space):
+        """
+        Remove the last comma in a string with or without spaces around comma.
+        """
+
+        only_comma = 1
+        comma_and_space = 2
+        amount_characters_skipped = only_comma
+
+        if with_extra_space:
+            amount_characters_skipped = comma_and_space
+
+        last_comma_index = sql_creation.rfind(',')
+        beginning_substring = sql_creation[0 : last_comma_index : ]
+        ending_substring = sql_creation[last_comma_index + amount_characters_skipped : :]
+        sql_creation = beginning_substring + ending_substring
+
+        return sql_creation
--- tests/Pokemon_Writer_Test.py
+++ tests/Pokemon_Writer_Test.py
@@ -2,8 +2,11 @@ import unittest
 import json
 from src.pokemon_writer import PokemonWriter
 
+Parsed_pokemon_data ="""{ "id ": 35,  "name ":  "clefairy ",  "height ": 6,  "weight ": 75,  "species ":  "Fairy Pokémon ",  "ability ":  "friend-guard ",  "primary_type ":  "fairy ",  "secondary_type ":  "none "}"""
+ 
 
 class Pokemon_Downloader_Test(unittest.TestCase):
+    
     
     def test_create_tables(self):
         pokemon_writer = PokemonWriter()
@@ -22,6 +25,13 @@ class Pokemon_Downloader_Test(unittest.TestCase):
 
         expected_result = "ALTER TABLE Pokemon ADD PRIMARY KEY (pokemon_name);\nALTER TABLE Moves ADD PRIMARY KEY (name);\n"
         self.assertEquals(expected_result, pokemon_sql)
+    
+    def test_create_primary_key(self):
+        pokemon_writer = PokemonWriter()
+        pokemon_sql = pokemon_writer.create_primary_key("pokemon_name", "Pokemon","")
+
+        expected_result = "ALTER TABLE Pokemon ADD PRIMARY KEY (pokemon_name);\n"
+        self.assertEquals(expected_result, pokemon_sql)
 
     def test_create_relationships(self):
         pokemon_writer = PokemonWriter()
@@ -34,19 +44,24 @@ class Pokemon_Downloader_Test(unittest.TestCase):
 
     def test_populate_table(self):
         pokemon_writer = PokemonWriter()
-        primary_keys = [("Moves", "pokemon_name","Pokemon","pokemon_name"),("Moves", "name","Types", "name")]
-        pokemon_sql = pokemon_writer.create_relationships(primary_keys,"")
+        pokemon_data_json = json.loads(Parsed_pokemon_data)
 
-        expected_result = "ALTER TABLE Moves ADD FOREIGN KEY (pokemon_name) REFERENCES Pokemon (pokemon_name);\nALTER TABLE Moves ADD FOREIGN KEY (name) REFERENCES Types (name);\n"
+        pokemon_sql = pokemon_writer.populate_table(pokemon_data_json,"Pokemon","")
+
+        expected_result = "INSERT INTO Pokemon VALUES (35, clefairy, 6, 75, Fairy Pokémon, friend-guard, fairy, none);"
 
         self.assertEquals(expected_result, pokemon_sql)
     
     def test_convert_pokemon_json_to_sql(self):
         pokemon_writer = PokemonWriter()
-        primary_keys = [("Moves", "pokemon_name","Pokemon","pokemon_name"),("Moves", "name","Types", "name")]
-        pokemon_sql = pokemon_writer.create_relationships(primary_keys,"")
+        pokemon_data_json = json.loads(Parsed_pokemon_data)
 
-        expected_result = "ALTER TABLE Moves ADD FOREIGN KEY (pokemon_name) REFERENCES Pokemon (pokemon_name);\nALTER TABLE Moves ADD FOREIGN KEY (name) REFERENCES Types (name);\n"
+        pokemon_sql = pokemon_writer.convert_pokemon_json_to_sql(pokemon_data_json)
+        expected_creation = "CREATE TABLE Pokemon (\tpokemon_name varchar,\n\tid int,\n\theight varchar,\n\tweight int,\n\tability varchar,\n\tspecies varchar,\n\tstats varchar,\n\tprimary_type varchar,\n\tsecondary_type varchar\n);\n"
+        expected_alter = "ALTER TABLE Pokemon ADD PRIMARY KEY (pokemon_name);\n"
+        expected_populate = "INSERT INTO Pokemon VALUES (35, clefairy, 6, 75, Fairy Pokémon, friend-guard, fairy, none);"
+
+        expected_result =expected_creation + expected_alter + expected_populate
 
         self.assertEquals(expected_result, pokemon_sql)
      
